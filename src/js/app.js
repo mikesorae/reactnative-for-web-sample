22b24e09232de84682ba434f036c19eeaebc58a6
mikesorae <lovepoko247@gmail.com>
2018-03-12 02:49:08 +0900

    sample routing

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom';
 import { View, Text } from 'react-native';
 import { ActivityIndicator, Button } from 'react-native';
 import Header from './components/header';
 
 const App = () => {
   return (
-    <View>
-      <Header />
-      <Text>Hello World!</Text>
-    </View>
+    <Router>
+      <View>
+        <Header />
+        <Text>Hello World!</Text>
+        <Link to='/'>to top</Link>
+        <Link to='/test'>to test</Link>
+        <Route exact path='/' component={()=>(<Text>top</Text>)} />
+        <Route path='/test' component={()=>(<Text>test</Text>)} />
+      </View>
+    </Router>
   )
 }
 
