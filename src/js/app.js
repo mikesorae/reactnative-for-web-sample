946b57198a1e65c6b73963db806115b489664ba4
mikesorae <lovepoko247@gmail.com>
2018-03-12 02:31:40 +0900

    sample

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { ActivityIndicator, Button } from 'react-native';
+import Header from './components/header';
 
 const App = () => {
   return (
     <View>
-      <Button title="test" />
+      <Header />
       <Text>Hello World!</Text>
     </View>
   )
