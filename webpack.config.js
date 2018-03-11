55c9d12cf2ff95bc591840fd970316da91a31c36
mikesorae <lovepoko247@gmail.com>
2018-03-11 23:51:50 +0900

    change entrypoint and add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: {
+    main: "./src/js/index.js"
+  },
   module: {
     rules: [
       {
@@ -7,7 +12,24 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 }
+
+module.exports = config;
